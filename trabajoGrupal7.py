9b87218a27aa3d32d70c4eb6eaf840fc0edd983c
DarkCamaleon <darkcamaleon@MacBook-Pro-de-Alejandro.local>
2021-10-21 21:47:49 -0300

    mi trabajo

--- trabajoGrupal7.py
+++ trabajoGrupal7.py
@@ -19,10 +19,27 @@ contactosTrabajadores = {}
 transacciones = {}
 
 productos = {}
+numero_contacto = int(input("cuantos contacto de trabajadores desea ingresar : "))
+
+
+contactosTrabajadores = {}
+
+for ingreso in range(0, numero_contacto):
+    rut= int(input("ingrese rut del contacto trabajador: "))
+    nombre= input("ingrese nombre del contacto trabajador: ")
+    apellido= input("ingrese apellido del contacto trabajador: ")
+    empresa= input("ingrese empresa que pertenece contacto trabajador: ")
+    cargo= input("ingrese cargo del contacto trabajador: ")
+    telefono= int(input("ingrese telefono del contacto trabajador: "))
+    email= input("ingrese email del contacto trabajador: ")
+    direccion= input("ingrese direccion del contacto trabajador: ")
+    contactosTrabajadores[rut]= [nombre,apellido,empresa,cargo,telefono,email,direccion]
+print(contactosTrabajadores)
+
 
 # proveedores: rut: nombre, telefono, email, dirección
 # contactosTrabajadores: rut: nombre, apellido, empresa proveedora, cargo, telefono, email, dirección
-# transacciones: idTransacción: fecha, productos, costo total. 
+# transacciones: idTransacción: fecha, productos, costo total.
 # producto: sku: nombre, costo unitario.
 
 
